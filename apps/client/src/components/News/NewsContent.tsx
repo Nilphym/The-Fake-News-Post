9eda934fb56826493320a02c87f3f1b8a99505df
Jędrzej Ratajczak <nilphym@gmail.com>
2022-06-04 22:05:09 +0200

    Finish game (#24)

--- apps/client/src/components/News/NewsContent.tsx
+++ apps/client/src/components/News/NewsContent.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import styles from './NewsContent.module.scss';
 
-export const NewsContent = ({ children }: { children: string }) => {
+export const NewsContent = ({
+  children,
+}: {
+  children: string | React.ReactNode;
+}) => {
   return <p className={styles.text}>{children}</p>;
 };
