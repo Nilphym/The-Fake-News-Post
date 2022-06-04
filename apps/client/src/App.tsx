5139991621dd9faec4f5c673f97a7b77b69923cc
LiveChat <livechat@MacBook-Pro-wojtek.local>
2022-06-05 00:50:14 +0200

    feat: added lobbymenuscreen

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout';
 import { RankPage } from './pages/RankPage';
 import { GamePage } from './pages/GamePage';
+import { GameLobby } from './components/GameLobby/GameLobby';
 
 export const App = () => {
   return (
@@ -9,6 +10,7 @@ export const App = () => {
       <Route path='/' element={<Layout />}>
         <Route index element={<div>Home Page</div>} />
         <Route path='game' element={<GamePage />} />
+        <Route path='host' element={<GameLobby />} />
         <Route path='summary' element={<div>Podsumowanie rozgrywki</div>} />
         <Route path='ranking' element={<RankPage />} />
       </Route>
