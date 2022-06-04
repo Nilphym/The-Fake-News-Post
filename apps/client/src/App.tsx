b37bd8c3f71400a9a89b2516ec07992b82ac3890
Jędrzej Ratajczak <nilphym@gmail.com>
2022-06-04 16:28:13 +0200

    Feature/main news (#12)
    * Add components for main news game
    
    * Add redux with game state
    
    * Add NewsPage to screen
    
    * Add yarn.lock
    
    * Edit tabWidth to 2

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,22 +1,9 @@
-import styles from './App.module.scss';
+import { NewsPage } from './pages/NewsPage';
 
 export const App = () => {
-    console.log(styles);
-    return (
-        <div className={styles.App}>
-            <header className={styles['App-header']}>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className={styles['App-link']}
-                    href='https://reactjs.org'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+  return (
+    <div style={{ margin: '0 auto', width: '40rem' }}>
+      <NewsPage />
+    </div>
+  );
 };
