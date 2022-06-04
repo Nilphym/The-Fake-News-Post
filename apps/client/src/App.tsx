f6825d0c1c59e0f8d80449c5a679654483b2e46c
LiveChat <livechat@MacBook-Pro-wojtek.local>
2022-06-04 17:38:33 +0200

    feat: HowToPlayComponent

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,23 +1,25 @@
 import { Routes, Route } from 'react-router-dom';
+import { HowToPlay } from './components/HowToPlay/HowToPlay';
 import { Layout } from './Layout';
 import { NewsPage } from './pages/NewsPage';
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Layout />}>
-        <Route index element={<div>Home Page</div>} />
-        <Route
-          path='game'
-          element={
-            <div style={{ margin: '0 auto', width: '40rem' }}>
-              <NewsPage />
-            </div>
-          }
-        />
-        <Route path='summary' element={<div>Podsumowanie rozgrywki</div>} />
-        <Route path='ranking' element={<div>Ranking</div>} />
-      </Route>
-    </Routes>
+    // <Routes>
+    //   <Route path='/' element={<Layout />}>
+    //     <Route index element={<div>Home Page</div>} />
+    //     <Route
+    //       path='game'
+    //       element={
+    //         <div style={{ margin: '0 auto', width: '40rem' }}>
+    //           <NewsPage />
+    //         </div>
+    //       }
+    //     />
+    //     <Route path='summary' element={<div>Podsumowanie rozgrywki</div>} />
+    //     <Route path='ranking' element={<div>Ranking</div>} />
+    //   </Route>
+    <HowToPlay />
+    // </Routes>
   );
 };
