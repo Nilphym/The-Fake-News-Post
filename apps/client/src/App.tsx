b860e2a6d1b4c1a7eb134d2c3c1e71b094f0599c
DawidZlobecki <96154754+DawidZlobecki@users.noreply.github.com>
2022-06-05 00:56:19 +0200

    Feature/start game screen part2 (#30)
    feat: added lobbymenuscreen
    
    Co-authored-by: LiveChat <livechat@MacBook-Pro-wojtek.local>

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout';
 import { RankPage } from './pages/RankPage';
 import { GamePage } from './pages/GamePage';
+import { GameLobby } from './components/GameLobby/GameLobby';
 import { StartPage } from './pages/startPage/StartPage';
 
 export const App = () => {
@@ -10,6 +11,7 @@ export const App = () => {
       <Route path='/' element={<Layout />}>
         <Route index element={<StartPage />} />
         <Route path='game' element={<GamePage />} />
+        <Route path='host' element={<GameLobby />} />
         <Route path='summary' element={<div>Podsumowanie rozgrywki</div>} />
         <Route path='ranking' element={<RankPage />} />
       </Route>
--- apps/client/src/utils/setSecondsToMinutes.ts
+++ apps/client/src/utils/setSecondsToMinutes.ts
@@ -1,10 +1,8 @@
 export const setSecondsToMinutes = (seconds: number) => {
-  let timeString: string;
+  const minutes = Math.floor(seconds / 60);
+  const secs = seconds % 60;
 
-  let minutes = Math.floor(seconds / 60);
-  let secs = seconds % 60;
-
-  timeString = `${minutes ? `${minutes} min` : '0 min'} ${
+  const timeString = `${minutes ? `${minutes} min` : '0 min'} ${
     secs ? `${secs} seconds` : '0 seconds'
   } `;
   return timeString;
