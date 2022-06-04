5669d649eedf35e2efd3447a05825984a1b56881
LiveChat <livechat@MacBook-Pro-wojtek.local>
2022-06-04 22:57:21 +0200

    feat: added highscore header component and fixed styles

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -10,7 +10,7 @@ export const App = () => {
         <Route index element={<div>Home Page</div>} />
         <Route path='game' element={<GamePage />} />
         <Route path='summary' element={<div>Podsumowanie rozgrywki</div>} />
-        <Route path='ranking' element={<RankPage/>} />
+        <Route path='ranking' element={<RankPage />} />
       </Route>
     </Routes>
   );
