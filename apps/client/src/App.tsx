543c62acb26253dcfeaf700d312ba8f0b937b274
Justyna Gładysz <61114674+jusgladysz@users.noreply.github.com>
2022-06-05 00:27:17 +0200

    Add StartPage component (#29)

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -2,12 +2,13 @@ import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout';
 import { RankPage } from './pages/RankPage';
 import { GamePage } from './pages/GamePage';
+import { StartPage } from './pages/startPage/StartPage';
 
 export const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
-        <Route index element={<div>Home Page</div>} />
+        <Route index element={<StartPage />} />
         <Route path='game' element={<GamePage />} />
         <Route path='summary' element={<div>Podsumowanie rozgrywki</div>} />
         <Route path='ranking' element={<RankPage />} />
