e2e9d6d44dca547c8b30839be4923b99cbbe85b8
LiveChat <livechat@MacBook-Pro-wojtek.local>
2022-06-04 19:49:07 +0200

    feat: added vertical borders

--- apps/client/src/Layout.tsx
+++ apps/client/src/Layout.tsx
@@ -4,6 +4,7 @@ import { Outlet } from 'react-router-dom';
 import { Authors } from './components/authors/Authors';
 import { Header } from './components/Header/Header';
 import { HowToPlay } from './components/HowToPlay/HowToPlay';
+import styles from "./Layout.module.scss"
 
 export const Layout = () => {
   return (
@@ -17,19 +18,14 @@ export const Layout = () => {
           <HowToPlay />
         </Col>
         <Col sm={{span: 12, order: 1}} md={{span: 6, order: 2}}>
-          <Outlet />
+          <div className={styles.verticalBorder}>
+            <Outlet />
+          </div>
         </Col>
         <Col sm={{span: 6, order: 3}} md={{span: 3, order: 3}}>
           <Authors />
         </Col>
       </Row>
     </Container>
-    //   <div>Nawigacja</div>
-    //   <div>
-    //     <div>Instrukcja / Lewy panel</div>
-    //     <Outlet />
-    //     <div>Autorzy + Rozgrywka / Prawy panel</div>
-    //   </div>
-    // </div>
   );
 };
